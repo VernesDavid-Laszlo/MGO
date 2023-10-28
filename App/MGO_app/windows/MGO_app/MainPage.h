01b5041296a5ad92aadc9d69a71cff8e75cc7055
Ervin28 <mihacsa.ervin@student.ms.sapientia.ro>
2023-10-28 18:54:31 +0300

    Project Initialization

---
+++ App/MGO_app/windows/MGO_app/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::MGO_app::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::MGO_app::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
