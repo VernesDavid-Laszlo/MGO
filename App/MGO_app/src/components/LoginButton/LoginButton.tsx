576c767ea5f5233928c7a677b371dfa29d33f546
Ervin28 <mihacsa.ervin@student.ms.sapientia.ro>
2023-12-06 14:02:27 +0200

    Firebase setup and implemented login with your credentials

--- App/MGO_app/src/components/LoginButton/LoginButton.tsx
+++ App/MGO_app/src/components/LoginButton/LoginButton.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 import {loginButtonStyle} from './LoginButtonStyle';
+import {CustomSignUpButtonProps} from '../SignUpButton/SignUpButton';
 
 export interface CustomLoginButtonProps {
   handlePress: () => void;
 }
 
-const CustomLoginButton = () => {
+const CustomLoginButton = ({handlePress}: CustomSignUpButtonProps) => {
   return (
     <TouchableOpacity
-      // onPress={handlePress}
+      onPress={handlePress}
       style={loginButtonStyle.buttonContainer}>
       <Text style={loginButtonStyle.buttonText}>Login</Text>
     </TouchableOpacity>
